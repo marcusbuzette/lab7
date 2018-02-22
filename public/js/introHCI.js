4f619e8e848311d5408a4b6eda56663dcac74e85
MarcusBuzette <marcusbuzete@me.com>
2018-02-22 10:53:36 -0800

    IN LAB

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(likeClick);
+}
+
+function likeClick(e){
+	e.preventDefault();
+	ga("create","UA-114577710-1","auto");
+	ga("send", "event", "like", "click");
 }
